ad87126edd2bf6c8f5f5f4fae53460d2d51d85d7
narisasingngam <mmintttt@gmail.com>
2019-12-08 13:35:24 +0700

    first commit

---
+++ src/components/InsurancePage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './../styles/insurancePage.css'
+
+export class InsurancePage extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="insu-page">
+                    <div className="user-input">
+                        Patient Detail
+                </div>
+                    <div className="user-status">
+                        ha
+                </div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default InsurancePage
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './../styles/nav.css'
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <div className="nav">
+                {/* f */}
+            </div>
+        )
+    }
+}
+
+export default Navbar
