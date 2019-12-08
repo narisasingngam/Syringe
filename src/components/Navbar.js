7d5ee63455d6a879e48bb3e9fc05b9d6518132fb
narisasingngam <mmintttt@gmail.com>
2019-12-08 20:59:04 +0700

    add route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import './../styles/nav.css'
+import logo from './../images/syringe.png'
+import { Link } from 'react-router-dom';
 
 export class Navbar extends Component {
     render() {
         return (
             <div className="nav">
-                {/* f */}
+                <Link to="/">
+                <img className="logo" src={logo} alt="Logo" />
+                </Link>
+                <Link to="/admin" className="btn-company">
+                {/* <i class="fas fa-search"></i> */}
+                    Search Company
+                </Link>
             </div>
         )
     }
