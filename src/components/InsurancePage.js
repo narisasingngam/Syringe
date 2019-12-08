d9f39a756764c993974f3b14f9a4fe7c43010879
narisasingngam <mmintttt@gmail.com>
2019-12-08 21:33:27 +0700

    add css'

--- src/components/InsurancePage.js
+++ src/components/InsurancePage.js
@@ -3,17 +3,22 @@ import './../styles/insurancePage.css'
 
 export class InsurancePage extends Component {
 
-    
+
 
     render() {
         return (
             <div className="container">
                 <div className="insu-page">
                     <div className="user-input">
-                        Patient detail
-                </div>
+                        <div className="input-pat-details">
+                            <h4>Patient insurance</h4>
+                            <div className="">
+                                <input className="input-id"></input>
+                            </div>
+                        </div>
+                    </div>
                     <div className="user-status">
-                    Patient status
+                        Patient status
                 </div>
                 </div>
             </div>
